c9fe54522f9cc9e5d1c725a8ad918dda7bc6a238
Albin jegus S <98178093+Albinjegus10@users.noreply.github.com>
2023-06-24 22:25:44 +0530

    Add files via upload

---
+++ curdbasicapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CurdbasicappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'curdbasicapp'
---
+++ curdbasicapp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import EmployeeDetails
+
+urlpatterns = [
+    path("emp/", EmployeeDetails.as_view(),name="emp"),
+
+]
